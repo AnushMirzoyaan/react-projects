e5f54030a54b9535d077a1fed31c96c4331a4d8a
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2023-02-15 21:40:54 +0400

    adding weather-app

---
+++ weather-app/src/Api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd219b99a80mshc79df323d501f7dp1184a9jsn857fbd46c372',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo" 
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "6b89c728d419771228b1f800d4e3671b"
